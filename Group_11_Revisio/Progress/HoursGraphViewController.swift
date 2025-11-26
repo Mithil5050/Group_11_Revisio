23f7130708a40874c86ba925ad2d990abade687f
ashikay495 <ashkay.495@gmail.com>
2025-11-26 17:51:12 +0530

    Progress Screen 1

---
+++ Group_11_Revisio/Progress/HoursGraphViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HoursGraphViewController.swift
+//  Group_11_Revisio
+//
+//  Created by Ashika Yadav on 26/11/25.
+//
+
+import UIKit
+
+class HoursGraphViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
